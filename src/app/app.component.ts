dff1b87dd85ee8623350f0937c8a53543c860341
Gian <giremio5@gmail.com>
2023-12-04 06:24:04 -0300

    api-retrieving-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,86 +1,142 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EChartsOption } from 'echarts';
 import { DatePipe } from '@angular/common';
 import { PlantStatusService } from './plant-status-service';
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  /**
-   *
-   */
-  constructor(private datePipe: DatePipe, private plantStatusService: PlantStatusService) {
-  }
+export class AppComponent implements OnInit, OnDestroy {
+  soilMoistureChartOption: EChartsOption = {};
+  waterLevelChartOption: EChartsOption = {};
+  distanceChartOption: EChartsOption = {};
+  activationsData: { date: Date }[] = [];
+
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private datePipe: DatePipe,
+    private plantStatusService: PlantStatusService
+  ) { }
+
   ngOnInit(): void {
-    this.plantStatusService.getHelloWorld().subscribe(r => console.log(r))
+    this.loadData(); // Initial data load
+
+    // Set up a timer to refresh data every minute
+    interval(60000) // 60000 milliseconds = 1 minute
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.loadData();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  loadData(): void {
+    this.plantStatusService.getAverageSoilMoisture().subscribe((result) => {
+      this.soilMoistureChartOption = this.createGaugeChart(result.average, 'Soil Moisture Level (%)');
+    });
+
+    this.plantStatusService.getAverageWaterLevel().subscribe((result) => {
+      this.waterLevelChartOption = this.createGaugeChart(result.average, 'Water Level (ml)');
+    });
+
+    this.plantStatusService.getAverageWaterLevel().subscribe((result) => {
+      this.distanceChartOption = this.createDistanceChart(result);
+    });
+
+    this.plantStatusService.getLastWaterPumpActivations().subscribe((result) => {
+      this.activationsData = result.map((activation: any) => ({
+        date: new Date(activation.date),
+      }));
+    });
+  }
+
+  createGaugeChart(value: number, title: string): EChartsOption {
+    return {
+      series: [
+       
+        {
+          type: 'gauge',
+          progress: {
+            show: true,
+            width: 18,
+          },
+          axisLine: {
+            lineStyle: {
+              width: 18,
+            },
+          },
+          axisTick: {
+            show: false,
+          },
+          splitLine: {
+            length: 15,
+            lineStyle: {
+              width: 2,
+              color: '#999',
+            },
+          },
+          axisLabel: {
+            distance: 25,
+            color: '#999',
+            fontSize: 10,
+          },
+          anchor: {
+            show: true,
+            showAbove: true,
+            size: 15,
+            itemStyle: {
+              borderWidth: 10,
+            },
+          },
+          title: {
+            show: true,
+            fontSize: 12,
+          },
+          detail: {
+            valueAnimation: true,
+            fontSize: 30,
+            offsetCenter: [0, '70%'],
+          },
+          data: [
+            {
+              value: value,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  createDistanceChart(data: { date: string; distance: number }[]): EChartsOption {
+    return {
+      xAxis: {
+        type: 'category',
+        data: data.map((item) => this.formatDateTime(new Date(item.date))),
+      },
+      yAxis: {
+        type: 'value',
+        name: 'Distance (cm)',
+      },
+      series: [
+        {
+          data: data.map((item) => item.distance),
+          type: 'line', // You can change this to 'bar' for a bar chart
+          smooth: true,
+        },
+      ],
+    };
   }
 
   formatDateTime(dateTime: Date): string {
     return this.datePipe.transform(dateTime, 'dd/MM/yyyy HH:mm:ss') || '';
   }
-
-  activationsData = [
-    { date: new Date('2023-01-01T10:30:00') },
-    { date: new Date('2023-02-05T15:45:00') },
-    { date: new Date('2023-03-10T08:15:00') },
-    // Add more activations as needed
-  ];
-
-
-  chartOption: EChartsOption = {
-    series: [
-      {
-        type: 'gauge',
-        progress: {
-          show: true,
-          width: 18
-        },
-        axisLine: {
-          lineStyle: {
-            width: 18
-          }
-        },
-        axisTick: {
-          show: false
-        },
-        splitLine: {
-          length: 15,
-          lineStyle: {
-            width: 2,
-            color: '#999'
-          }
-        },
-        axisLabel: {
-          distance: 25,
-          color: '#999',
-          fontSize: 10
-        },
-        anchor: {
-          show: true,
-          showAbove: true,
-          size: 15,
-          itemStyle: {
-            borderWidth: 10
-          }
-        },
-        title: {
-          show: false
-        },
-        detail: {
-          valueAnimation: true,
-          fontSize: 30,
-          offsetCenter: [0, '70%']
-        },
-        data: [
-          {
-            value: 70
-          }
-        ]
-      }
-    ]
-  }
-
-  title = 'plant-visualization';
 }
